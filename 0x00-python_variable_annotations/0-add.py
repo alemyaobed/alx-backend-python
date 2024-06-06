b718d298a538472052f3083cf5eae38daf151d51
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-06-06 22:38:07 +0000

    implementation of task 2 and some minor changes to the files in terms of documentations

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -7,5 +7,5 @@ sum as a float.
 
 
 def add(a: float, b: float) -> float:
-    # Takes two float arguments and returns their sum as a float
+    '''Takes two float arguments and returns their sum as a float'''
     return a + b
--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -6,5 +6,5 @@ a string str2 as arguments and returns a concatenated string
 
 
 def concat(str1: str, str2: str) -> str:
-    # Concatenates two strings
+    ''' Concatenates two strings'''
     return str1 + str2
