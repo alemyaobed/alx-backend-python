ff206181f963c936bcfaac42afbaf36e1a5acf95
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-06-10 17:49:41 +0000

    pycodestyle and documentation style

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -10,11 +10,12 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    '''The implementation'''
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = []
 
     for task in asyncio.as_completed(tasks):
         delay = await task
         delays.append(delay)
-    
+
     return delays
