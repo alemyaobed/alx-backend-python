919d67b03ad83a0fec1f3a2eaecff782bbfa52fa
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-06-11 18:05:37 +0000

    the implementation of task 2 and some minor changes to the other files

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,10 +7,13 @@ second, then yield a random number between 0 and 10. Use the random module.
 '''
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
-    '''The implementation of the function'''
+async def async_generator() -> Generator[float, None, None]:
+    """ loop 10 times and in each asynchronously wait 1 second
+        then yield a random number between 0 and 10
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.random() * 10
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python3
+'''
+Import async_comprehension from the previous file and write a measure_runtime
+coroutine that will execute async_comprehension four times in parallel using
+asyncio.gather.
 
+measure_runtime should measure the total runtime and return it.
+
+Notice that the total runtime is roughly 10 seconds, explain it to yourself.
+'''
 import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-measure_runtime = __import__('2-measure_runtime').measure_runtime
+async def measure_runtime() -> float:
+    """ executes comprehension 4 times in parallel, returns runtime """
 
-
-async def main():
-    return await(measure_runtime())
-
-print(
-    asyncio.run(main())
-)
+    start = time.perf_counter()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    stop = time.perf_counter()
+    return stop - start
