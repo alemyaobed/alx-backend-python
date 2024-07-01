da288040021b42617b3be43aff10fa9b7feea349
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-07-01 10:32:47 +0000

    the implementation of task 0

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,19 +1,45 @@
 #!/usr/bin/env python3
 '''
-Familiarize yourself with the utils.access_nested_map function and understand its purpose. Play with it in the Python console to make sure you understand.
+Familiarize yourself with the utils.access_nested_map function and understand
+its purpose. Play with it in the Python console to make sure you understand.
 
 In this task you will write the first unit test for utils.access_nested_map.
 
 Create a TestAccessNestedMap class that inherits from unittest.TestCase.
 
-Implement the TestAccessNestedMap.test_access_nested_map method to test that the method returns what it is supposed to.
+Implement the TestAccessNestedMap.test_access_nested_map method to test that
+the method returns what it is supposed to.
 
-Decorate the method with @parameterized.expand to test the function for following inputs:
+Decorate the method with @parameterized.expand to test the function for
+following inputs:
 
 nested_map={"a": 1}, path=("a",)
 nested_map={"a": {"b": 2}}, path=("a",)
 nested_map={"a": {"b": 2}}, path=("a", "b")
-For each of these inputs, test with assertEqual that the function returns the expected result.
+For each of these inputs, test with assertEqual that the function returns the
+expected result.
 
 The body of the test method should not be longer than 2 lines.
 '''
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    '''
+    A class for the access_nested_map function in the utils module
+    '''
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, result):
+        ''' test case for the function using the parameterized '''
+        self.assertEqual(access_nested_map(
+            nested_map=nested_map, path=path), result)
+
+
+if __name__ == "__main__":
+    unittest.main()
